9989721d73956fcd30c3a2db249191bfbb8142b9
Pedro Bastos <pedro@pontonet.com>
2019-10-02 08:42:25 -0400

    w3

--- web/week03/backend/form-handler.php
+++ web/week03/backend/form-handler.php
@@ -3,11 +3,12 @@
 $majors = json_decode(file_get_contents('majors.json'));
 $continents = json_decode(file_get_contents('continents.json'));
 
-$request_continents = filter_input(INPUT_POST, 'continents' );
+$request_continents = filter_input(INPUT_POST, 'continents[]' );
 
 $response_continents = [];
 var_dump($request_continents);
 var_dump($continents);
+var_dump($majors);
 foreach($request_continents as $c)
 {
     if( in_array($c['id'], $continents) )
