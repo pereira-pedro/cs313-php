9f936fabd5232e38ecadb5448de6875aacd0464e
Pedro Bastos <pedro@pontonet.com>
2019-10-01 20:19:49 -0400

    Week 03

--- web/week03/backend/form-handler.php
+++ web/week03/backend/form-handler.php
@@ -1,9 +1,9 @@
 <?php
 $response = [
-    'name' => filter_input(INPUT_POST, 'name' ),
-    'email' => filter_input(INPUT_POST, 'email' ),
-    'major' => filter_input(INPUT_POST, 'major' ),
-    'comments' => filter_input(INPUT_POST, 'comments' )
+    'Name' => filter_input(INPUT_POST, 'name' ),
+    'Email' => filter_input(INPUT_POST, 'email' ),
+    'Major' => filter_input(INPUT_POST, 'major' ),
+    'Comments' => filter_input(INPUT_POST, 'comments' )
 ];
 
 header('Content-Type: application/json');
