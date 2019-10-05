275cf95ce178e35393630266cc1302b54b50c603
Pedro Bastos <pedro@pontonet.com>
2019-10-05 00:16:04 -0400

    w

--- web/week03/prove/backend/product-add.php
+++ web/week03/prove/backend/product-add.php
@@ -25,6 +25,7 @@ if ($status === 'OK') {
     // search if product already exists in cart
     $productIndex = array_search($id, array_column($cart['items'], 'id'));
     var_dump(array_column($cart['items'], 'id'));
+    echo "productIndex: $productIndex";
     // if not exists update quantity
     if ($productIndex !== false) {
         // remove if qty is 0
