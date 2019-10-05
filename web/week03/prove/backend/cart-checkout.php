c727623aee71b3a5c23cf02461b082bfbc08cfd4
Pedro Bastos <pedro@pontonet.com>
2019-10-05 09:21:19 -0400

    w

--- web/week03/prove/backend/cart-checkout.php
+++ web/week03/prove/backend/cart-checkout.php
@@ -21,7 +21,7 @@ unset($_SESSION['cart']);
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
         integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
-    <link rel="stylesheet" href="main.css" />
+    <link rel="stylesheet" href="../main.css" />
 
     <title>Grampos Store</title>
 </head>
@@ -30,7 +30,7 @@ unset($_SESSION['cart']);
     <!-- Navigation -->
     <nav class="navbar navbar-expand-lg navbar-light bg-light shadow fixed-top">
         <div class="container">
-            <a class="navbar-brand" href="index.html"><img src="images/logo.svg" width="80" height="80" /></a>
+            <a class="navbar-brand" href="index.html"><img src="../images/logo.svg" width="80" height="80" /></a>
             <h1>Grampos</h1>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive"
                 aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
@@ -39,7 +39,7 @@ unset($_SESSION['cart']);
             <div class="collapse navbar-collapse" id="navbarResponsive">
                 <ul class="navbar-nav ml-auto">
                     <li class="nav-item">
-                        <a href="index.html" class="btn btn-primary">
+                        <a href="../index.html" class="btn btn-primary">
                             Continue Shopping
                         </a>
                     </li>
@@ -57,8 +57,8 @@ unset($_SESSION['cart']);
             </p>
         </div>
         <div class="container">
-            <h1>Your order <?php echo $cart['id'] ?> was placed.</h1>
-            <h2>See the details below</h2>
+            <h1 class="m-3">Your order <?php echo $cart['id'] ?> was placed.</h1>
+            <h4 class="m-2">See the details below</h4>
         </div>
         <div class="container">
             <div class="row table-responsive">
@@ -75,10 +75,12 @@ unset($_SESSION['cart']);
                         <?php
             foreach ($cart as $item) {
               ?>
-                        <td class="p-2"><?php echo $item['title'] ?></td>
-                        <td class="p-2 text-center"><?php echo $item['qty'] ?></td>
-                        <td class="p-2 text-right"><?php echo $item['price'] ?></td>
-                        <td class="p-2 text-right"><?php echo $item['price'] * $item['qty'] ?></td>
+                        <tr>
+                            <td class="p-2"><?php echo $item['title'] ?></td>
+                            <td class="p-2 text-center"><?php echo $item['qty'] ?></td>
+                            <td class="p-2 text-right"><?php echo $item['price'] ?></td>
+                            <td class="p-2 text-right"><?php echo $item['price'] * $item['qty'] ?></td>
+                        </tr>
                         <?php
             }
             ?>
@@ -94,39 +96,19 @@ unset($_SESSION['cart']);
             </div>
         </div>
         <div class="container">
-            <div class="row">
-                <div class="col">
-                    Address
-                </div>
-                <div class="col">
-                    <?php echo $address1 ?>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col">
-                    Apartment/Suite/Room
-                </div>
-                <div class="col">
-                    <?php echo $address2 ?>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col">
-                    City
-                </div>
-                <div class="col">
-                    <?php echo $city ?>
-                </div>
-                <div class="col">
-                    <?php echo $state ?>
-                </div>
-                <div class="col">
-                    <?php echo $zip ?>
-                </div>
-            </div>
+            <h4 class="m-2">The order will be delivered to</h4>
         </div>
         <div class="container">
-            <a href="index.html">Continue Shopping</a>
+
+            <?php echo $address1 ?>
+            <?php echo $address2 ?>
+            <?php echo $city ?>
+            <?php echo $state ?>
+            <?php echo $zip ?>
+        </div>
+        </div>
+        <div class="container">
+            <a href="../index.html">Continue Shopping</a>
         </div>
         </div>
     </section>
