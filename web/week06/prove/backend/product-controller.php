e010b35a2cb2938b7feae22fe7dd8a3f4f39dbf5
Pedro Bastos <pedro@pontonet.com>
2019-10-26 09:21:26 -0400

    Week 06

---
+++ web/week06/prove/backend/product-controller.php
@@ -0,0 +1,41 @@
+<?php
+include_once 'utils.php';
+include_once 'models/Product.php';
+
+session_start();
+$action = filter_input(INPUT_POST, 'action');
+
+try {
+    $model = new Product();
+
+    $product = Utils::getPostObject($model->getDefinition());
+
+    switch ($action) {
+        case 'save':
+            if ($product->id === null) {
+                $model->create($product);
+                $message = "Product '$product->title' was created.";
+            } else {
+                $model->update($product);
+                $message = "Product '$product->title' was updated.";
+            }
+            break;
+        case 'delete':
+            $model->delete($product->id);
+            $message = "Product '$product->title' was deleted.";
+            break;
+    }
+    $response = [
+        'status' => 'OK',
+        'message' => $message
+    ];
+} catch (Exception $ex) {
+    $response = [
+        'status' => 'FAIL',
+        'message' => $ex->getMessage()
+    ];
+}
+
+
+header('Content-Type: application/json');
+echo json_encode($response);
---
+++ web/week06/prove/js/product.js
@@ -0,0 +1,57 @@
+const formatter = new Intl.NumberFormat("en-US", {
+  style: "currency",
+  currency: "USD"
+});
+
+const Toast = Swal.mixin({
+  toast: true,
+  position: "top-end",
+  showConfirmButton: false,
+  timer: 3000
+});
+
+/**
+ * This is a shortcut to jQuery ready function. It's called right after DOM is loaded and ready.
+ */
+$(function() {
+  $("#frm-product").submit(function(event) {
+    event.preventDefault();
+    const description = {
+      title: $("#det-title").val(),
+      subtitle: $("#det-subtitle").val(),
+      details: []
+    };
+
+    $("#product-features li").each(function() {
+      description.details.push($(this).text());
+    });
+
+    $("#description").val(description);
+
+    $("#action").val("save");
+
+    $.post("backend/product-controller.php", $(this).serialize(), function(
+      response
+    ) {
+      if (response.status !== "OK") {
+        Toast.fire({
+          type: "success",
+          title: response.message
+        });
+      } else {
+        Swal.fire({
+          type: "error",
+          title: "Error",
+          text: response.message
+        });
+      }
+    });
+  });
+  $("#btn-det-feature").click(function() {
+    $("#product-features").append(
+      $("<li>")
+        .class("list-group-item")
+        .text($("#det-feature").val())
+    );
+  });
+});
